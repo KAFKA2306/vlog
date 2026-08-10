ed8c10f889b0a6e81ff482d25d6cff3c2b94a433
KAFKA <137051370+KAFKA2306@users.noreply.github.com>
2026-08-10 21:53:10 +0900

    Export companion memory API

---
+++ packages/companion/src/vlog_companion/__init__.py
@@ -0,0 +1,11 @@
+from .core import TermMemory, choose, encode_reading, katakana, observe, synced_parameter_bits, weight
+
+__all__ = [
+    "TermMemory",
+    "choose",
+    "encode_reading",
+    "katakana",
+    "observe",
+    "synced_parameter_bits",
+    "weight",
+]
